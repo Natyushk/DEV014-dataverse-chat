1d129ce7c7d5c4c5147a4a1fda4450a34a4b97dd
Natyushk <136663795+Natyushk@users.noreply.github.com>
2024-04-17 17:26:03 -0500

    Refactorizar home.js compirtamiento de filtros, botones y fortune stats.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,13 @@
-import  data  from '../Data/dataset.js';
-import  {header}  from '../Components/header.js';
+import data from '../Data/dataset.js';
+import { header } from '../Components/header.js';
 import { renderItems } from '../Components/layoutCards.js';
-//import { filterByContinent, sortBy, calculateFortuneStats, filterByName} from '../lib/dataFunctions.js';
+import { filterByContinent, sortBy, calculateFortuneStats, filterByName } from '../lib/dataFunctions.js';
 import divStats from '../Components/divStats.js';
 import searchSection from '../Components/searchSection.js';
 import footer from '../Components/footer.js';
 
 //import saveApiKey from '../Components/saveApiKey.js';
-//import noCoincidences from '../Components/noCoincidences.js';
+import noCoincidences from '../Components/noCoincidences.js';
 
 export function Home() {
   //Llamar componente header
@@ -22,31 +22,13 @@ export function Home() {
   homeContainer.appendChild(footer());
 
   //sección de comportamiento
-  const resetButton = homeContainer.querySelector('#reset-button'); 
-
-  resetButton.addEventListener('click', () => {
-    console.log("Hola");
-  })
-
-
-  //Mostrar Modal
-  //dialogApiKey.showModal();
-
-
-  /*const continentFilterSelect = main.querySelector('#continent-filter');
-  const sortBySelect = main.querySelector('#sort-by');
-  const totalFortuneElement = main.querySelector('#total-fortune');
-  const averageFortuneElement = main.querySelector('#average-fortune');
-  const searchInput = main.querySelector('#search-input');
-  const resetButton = main.querySelector('#reset-button');
-  const saveButton = body.querySelector('#save-apikey');
-
-  let richPeopleList = document.querySelector("main").appendChild(renderItems(sortBy(data, 'asc')));
-  saveButton.addEventListener('click', () => {
-    const inputApiKey = body.querySelector('#input-api-key').value;
-    localStorage.setItem('API Key', inputApiKey)
-    dialogApiKey.close();
-  });
+  const continentFilterSelect = homeContainer.querySelector('#continent-filter');
+  const sortBySelect = homeContainer.querySelector('#sort-by');
+  const totalFortuneElement = homeContainer.querySelector('#total-fortune');
+  const averageFortuneElement = homeContainer.querySelector('#average-fortune');
+  const searchInput = homeContainer.querySelector('#search-input');
+  const resetButton = homeContainer.querySelector('#reset-button');
+  //const saveButton = homeContainer.querySelector('#save-apikey');
 
   // Función para actualizar y mostrar la fortuna total y el promedio
   const updateFortuneStats = (data) => {
@@ -54,29 +36,31 @@ export function Home() {
     totalFortuneElement.textContent = `Suma total de fortunas: $${formattedTotalFortune.toLocaleString('en')} B`;
     averageFortuneElement.textContent = `Promedio de fortunas: $${isNaN(formattedAverageFortune) ? 0 : formattedAverageFortune} B`;
   };
-  
+
   // Actualizar las estadísticas de fortuna inicialmente
   updateFortuneStats(data);
-  
+
   // Manejador de evento para cambio en select para filtrar por continente
   continentFilterSelect.addEventListener('change', () => {
     const continent = continentFilterSelect.value;
     const filteredData = filterByContinent(data, continent);
     sortBySelect.value = 'asc';
-    richPeopleList.remove();
-    richPeopleList = document.querySelector("main").appendChild(renderItems(filteredData));
+    cardsContainer.innerHTML = '';
+    cardsContainer.appendChild(renderItems(filteredData));
     updateFortuneStats(filteredData);
+    searchInput.value = ''
   });
-  
+
   // Manejador de evento para cambio en select para ordenar
   sortBySelect.addEventListener('change', () => {
     const order = sortBySelect.value;
-    const sortedData = sortBy(filterByContinent(data,continentFilterSelect.value), order);
-    richPeopleList.remove();
-    richPeopleList = document.querySelector("main").appendChild(renderItems(sortedData));
+    const sortedData = sortBy(filterByContinent(data, continentFilterSelect.value), order);
+    cardsContainer.innerHTML = '';
+    cardsContainer.appendChild(renderItems(sortedData));
     updateFortuneStats(sortedData);
+    searchInput.value = ''
   });
-  
+
   //Manejador de evento para cambio en input de búsqueda
   searchInput.addEventListener('input', () => {
     //Obtener el valor del input
@@ -84,23 +68,35 @@ export function Home() {
     //Filtrar los datos según el valor del input
     const filteredData = filterByName(data, searchTerm);
     //Renderizar los resultados filtrados
-    /*if (filteredData.length === 0) {
-      main.appendChild(noCoincidences());
-    } */ //Dentro de la función se ejecuta cada vez que el input cambia 
-    
-  //   richPeopleList.remove();
-  //   richPeopleList = document.querySelector("main").appendChild(renderItems(filteredData));
-  //   updateFortuneStats(filteredData);
-  // })  
+    if (filteredData.length === 0) {
+      cardsContainer.innerHTML = ''
+      cardsContainer.appendChild(noCoincidences())
+    }
+    else {
+      cardsContainer.innerHTML = ''
+      cardsContainer.appendChild(renderItems(filteredData))
+      updateFortuneStats(filteredData);
+    }
+  }); //Dentro de la función se ejecuta cada vez que el input cambia 
 
   //Función para boton limpiar
-  // resetButton.addEventListener('click', () => {
-  //   continentFilterSelect.value = 'Todos';
-  //   sortBySelect.value = 'asc';
-  //   richPeopleList.remove();
-  //   richPeopleList = document.querySelector("main").appendChild(renderItems(sortBy(data, 'asc')));
-  //   updateFortuneStats(data);
-  // });¨*/
+  resetButton.addEventListener('click', () => {
+    continentFilterSelect.value = 'Todos';
+    sortBySelect.value = 'asc';
+    cardsContainer.innerHTML = '';
+    cardsContainer.appendChild(renderItems(sortBy(data, 'asc')));
+    updateFortuneStats(data);
+    searchInput.innerHTML = '';
+  });
+  /*saveButton.addEventListener('click', () => {
+    const inputApiKey = homeContainer.querySelector('#input-api-key').value;
+    localStorage.setItem('API Key', inputApiKey)
+    dialogApiKey.close();
+  });*/
+
+
+  //Mostrar Modal
+  //dialogApiKey.showModal();
 
   return homeContainer;
 }
