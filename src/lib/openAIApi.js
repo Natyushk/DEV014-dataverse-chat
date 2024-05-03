c2d6cb25f5dd5eba51b428cd21b39ec7e5a9ace7
sandymmora <sandy.mmora@gmail.com>
2024-05-03 17:17:53 -0600

    Chat individual funcionando con respuesta de Open AI

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,32 +1,39 @@
-// Importa la función para obtener la API KEY desde apiKey.js
+
 import { getApiKey } from './apikey.js';
 
-export const communicateWithOpenAI = (messages) => {
-  const apiKey = getApiKey();
-  const url = 'https://api.openai.com/v1/chat/completions';
-  const requestBody = {
-    messages: messages,
-    model: "gpt-3.5-turbo",
-    temperature: 0.7,
-  };
+export const communicateWithOpenAI = (richPerson, userMessage) => {
 
-  const response = fetch(url, {
+  const apiKey = getApiKey();
+
+  const response = fetch('https://api.openai.com/v1/chat/completions', {
     method : "POST",
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${apiKey}`
     },
-    body: JSON.stringify(requestBody)
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: 'system',
+          content:`You are a ${richPerson}`
+        },
+        {
+          role: 'user',
+          content: `${userMessage}`
+        }]
+    })
   });
 
   return response.then(res =>{
     if (!res.ok){
-      throw new Error('La respuesta no esta correcta');
+      throw new Error('La solicitud falló');
     }
     const data = res.json();
     return data;
+
   }).catch(err => {
-    console.error('Error de fetch:', err);
+    //console.error(err);
     return err;
   })
 };
