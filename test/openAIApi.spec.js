f52b6549468d58dbf9862f5bf5591f97927cb55a
sandymmora <sandy.mmora@gmail.com>
2024-05-13 12:05:52 -0600

    Test

--- test/openAIApi.spec.js
+++ test/openAIApi.spec.js
@@ -1,14 +1,15 @@
 import { communicateWithOpenAI } from '../src/lib/openAIApi.js';
 
-// global.fetch = jest.fn(() => 
-//   Promise.resolve({
-//     json: () => Promise.resolve({ message: 'Tengo 37 años' }),
-//   })
-// );
-// test('communicateWithOpenAI devuelve los datos correctamente', async () => {
-//   const data = await communicateWithOpenAI('Mark Zuckerberg', '¿Que edad tienes?');
-//   expect(data).toEqual({ message: 'Tengo 37 años' });
-// });
+global.fetch = jest.fn(() => 
+  Promise.resolve({
+    json: () => Promise.resolve({ message: 'Tengo 37 años' }),
+  })
+);
+
+test('communicateWithOpenAI devuelve los datos correctamente', async () => {
+  const data = await communicateWithOpenAI('Mark Zuckerberg', '¿Que edad tienes?');
+  expect(data).toEqual({ message: 'Tengo 37 años' });
+});
 
 describe('communicateWithOpenAI', () => {
   test('Debería obtener una respuesta válida de OpenAI', () => {
