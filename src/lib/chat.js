758a319838f0934a3df23059987cc886e1a0e867
Natyushk <136663795+Natyushk@users.noreply.github.com>
2024-05-14 19:39:23 -0500

    Realizar las pruebas unitarias de la integración de pruebas unitarias

--- src/lib/chat.js
+++ src/lib/chat.js
@@ -6,16 +6,17 @@ import nodeMessage from "../Components/message.js";
 //Función para enviar un mensaje al servidor 
 export function showMessages(prompInput, chatContainer, namePerson){
   const prompUser = prompInput.value;
+  console.log('2');
   chatContainer.appendChild(nodeMessage('user', prompUser));
   prompInput.value = "";
   return communicateWithOpenAI(namePerson, prompUser).then(response => {
     const responseMessage = response.choices[0].message.content;
+    console.log('3');
     //Mostrar la respsuesta de Open Ai en el Chat
     chatContainer.appendChild(nodeMessage('apiResponse', responseMessage, namePerson));
     chatContainer.scrollTop = chatContainer.scrollHeight;
   }).catch(error => {
-    const errorMessage = error;
-    chatContainer.appendChild(nodeMessage('error', errorMessage, 'Lo sentimos'));
+    chatContainer.appendChild(nodeMessage('error', error.message, 'Lo sentimos'));
   })
 }
 export default showMessages;
